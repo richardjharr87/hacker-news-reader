d89983b8961458e07b5a26cfaef1ae12df20ff4f
richardjharr87 <66270343+richardjharr87@users.noreply.github.com>
2020-06-22 10:31:38 -0700

    Updates

---
+++ precache-manifest.bd59fcc4c7313710348ecc69c94faa91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d9d5310eb180d75f2a9a8de70d9e9d",
+    "url": "/hacker-news-reader/index.html"
+  },
+  {
+    "revision": "52d69b619b8641919de7",
+    "url": "/hacker-news-reader/static/js/2.106b5c6d.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-reader/static/js/2.106b5c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5250f61e4e17a2086e7",
+    "url": "/hacker-news-reader/static/js/main.6c958961.chunk.js"
+  },
+  {
+    "revision": "15cab9a75f106f9b64b9",
+    "url": "/hacker-news-reader/static/js/runtime-main.20d9bca5.js"
+  }
+]);
